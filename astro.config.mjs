05e15bf7e7678e28f963a1493dff760f1d23e881
darwincrack <erudito_good@hotmail.com>
2025-06-24 18:44:31 -0400

    Actualiza la configuración de Astro para utilizar el adaptador de Vercel y cambiar la salida a 'server'. Se añaden nuevas dependencias en package.json, incluyendo '@astrojs/vercel' y se actualizan versiones de varias dependencias, mejorando la compatibilidad y el rendimiento del proyecto.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +12,12 @@ export default defineConfig({
       include: ['tsparticles', 'tsparticles-engine'],
     },
   },
+
+  experimental: {
+    liveContentCollections: true,
+  },
   site: 'https://darwincd.com',
   integrations: [sitemap()],
-  output: 'hybrid'
+  output: 'server',
+  adapter: vercel(),
 });
